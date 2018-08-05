8bd290f5d094d9b551bbfb22e9e3d027c7d739d2
Evelyn Galvez <evelyn.galvez.p@gmail.com>
2018-08-05 02:47:11 -0400

    agregado input Añadir lista

--- src/js/main.js
+++ src/js/main.js
@@ -2,4 +2,5 @@ const btn_add_list = document.getElementById('btn_add_list');
 
 btn_add_list.addEventListener('click', () => {
   btn_add_list.style.display = 'none';
+  input_add_list.style.display = 'block';
 })
