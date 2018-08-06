2dd2671db59ae8345c7c97496be55b4d79fdc02e
Evelyn Galvez <evelyn.galvez.p@gmail.com>
2018-08-06 14:59:09 -0400

    añade tareas, generado mediante DOM

--- src/js/main.js
+++ src/js/main.js
@@ -3,18 +3,41 @@ const btn_add_list = document.getElementById('btn_add_list');
 btn_add_list.addEventListener('click', () => {
   btn_add_list.style.display = 'none';
   input_add_list.style.display = 'block';
+  container_task.style.display = 'none';
 })
 
 const btn_guardar = document.getElementById('btn_guardar');
 
+
 btn_guardar.addEventListener('click', () => {
   let listName = document.getElementById('formGroupExampleInput').value;
   console.log(listName);
-  formGroupExampleInput.style.display = 'none';
-  btn_guardar.style.display = 'none';
-  close_icon.style.display = 'none';
-  document.getElementById('container_task').style.display = 'block';
-  document.getElementById('list_name').innerHTML = listName;
-  document.getElementById('container_textarea').style.display = 'block';
-  document.getElementById('add_task').style.display = 'none';
+  container_list.style.display = 'none';
+  container_task.style.display = 'block';
+  list_task.style.display = 'none';
+  list_name.innerHTML = listName;
+  container_textarea.style.display = 'none';
+
+  const addTask = document.getElementById('add_task');
+  addTask.addEventListener('click', () => {
+    console.log('boton añadir una tarea si funciona');
+    container_textarea.style.display = 'block';
+    list_task.style.display = 'block';
+    add_task.style.display = 'none';
+
+    const addTitleTask = document.getElementById('btn_añadir');
+    addTitleTask.addEventListener('click', () => {
+      console.log('boton añadir titulo si funciona');
+      let text = document.getElementById('task').value;
+      console.log('>>>>>>>>>>>>>>>>');
+      console.log(text);
+      task.value = '';
+      let newDiv = document.createElement('div');
+      let newTask = document.createTextNode(text);
+      newDiv.appendChild(newTask);
+      let containerElements = document.getElementById('list_task');
+      containerElements.appendChild(newDiv);
+    })
+  })
 })
+
