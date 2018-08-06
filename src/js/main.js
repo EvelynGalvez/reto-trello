d664383f6b82f46d28b7be9710f2ada95ad83ecd
Evelyn Galvez <evelyn.galvez.p@gmail.com>
2018-08-06 12:45:58 -0400

    guarda titulo tarjeta y agrega enlace añadir una tarea

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,15 @@ btn_add_list.addEventListener('click', () => {
   btn_add_list.style.display = 'none';
   input_add_list.style.display = 'block';
 })
+
+const btn_guardar = document.getElementById('btn_guardar');
+
+btn_guardar.addEventListener('click', () => {
+  let listName = document.getElementById('formGroupExampleInput').value;
+  console.log(listName);
+  formGroupExampleInput.style.display = 'none';
+  btn_guardar.style.display = 'none';
+  close_icon.style.display = 'none';
+  document.getElementById('container_task').style.display = 'block';
+  document.getElementById('list_name').innerHTML = listName;
+})
